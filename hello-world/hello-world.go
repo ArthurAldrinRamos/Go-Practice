6cd825e0466a0a2774338602bb991b739c45f1ec
Arthur Aldrin Ramos <arthuraldrinramos@gmail.com>
2023-09-13 13:17:06 +0800

    Practice

--- hello-world/hello-world.go
+++ hello-world/hello-world.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func ipagDikit(s1, s2 string) string {
 	return s1 + s2
@@ -8,20 +10,59 @@ func ipagDikit(s1, s2 string) string {
 
 func main(){
 
+	// fmt.Println("Hello World")
 
-	firstNum := 1
-	secondNum := 2
+	// firstNum := 1
+	// secondNum := 2
 
-	firstName := "Arthur "
-	lastName := "Ramos"
+	// firstName := "Arthur "
+	// lastName := "Ramos"
 
-	if (firstNum > secondNum) {
-		fmt.Println("First Number is greater than the Second Number")
-	} else {
-		fmt.Println("Mehhh")
+	// if (firstNum > secondNum) {
+	// 	fmt.Println("First Number is greater than the Second Number")
+	// } else {
+	// 	fmt.Println("Mehhh")
+	// }
+
+	// fmt.Println(ipagDikit(firstName, lastName))
+
+	type ninja struct {
+
+		name string
+		weapons []string
+		level int
+
 	}
 
-	fmt.Println(ipagDikit(firstName, lastName))
+	arthur := ninja {
+
+		name: "Arthur",
+		weapons: []string{"Katana", "Bow and Arrows", "Ninja Star"},
+		level: 1,
+	
+	}
+
+	type dojo struct {
+
+		name string
+		ninja ninja
+
+	}
+
+
+	arthurDojo := dojo {
+
+		name: "Art Dojo",
+		ninja: arthur,
+
+	}
+
+	fmt.Println(arthur)
+	fmt.Println(arthur.name)
+	fmt.Printf("His weapons are: %v\n", arthur.weapons)
+	fmt.Printf("He is Level: %v\n", arthur.level)
+	fmt.Printf(arthurDojo.ninja.name)
+
 
 	// fmt.Println(firstNum, secondNum)
 	
