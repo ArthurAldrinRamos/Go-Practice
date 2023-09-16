ad44ea8c7ca3f7b6404cc2bb875be603026e4a76
Arthur Aldrin Ramos <arthuraldrinramos@gmail.com>
2023-09-16 18:35:56 +0800

    Go Practice: Classes and Pointers

--- hello-world/hello-world.go
+++ hello-world/hello-world.go
@@ -8,23 +8,34 @@ func ipagDikit(s1, s2 string) string {
 	return s1 + s2
 }
 
+
+type catType struct {
+	name string
+	color string
+	traits []string
+}
+
+func (c catType) catScratch() {
+	fmt.Printf("%v scratched you!!!", c.name)
+}
+
 func main(){
 
-	// fmt.Println("Hello World")
+	fmt.Println("Hello World")
 
-	// firstNum := 1
-	// secondNum := 2
+	firstNum := 1
+	secondNum := 2
 
-	// firstName := "Arthur "
-	// lastName := "Ramos"
+	firstName := "Arthur "
+	lastName := "Ramos"
 
-	// if (firstNum > secondNum) {
-	// 	fmt.Println("First Number is greater than the Second Number")
-	// } else {
-	// 	fmt.Println("Mehhh")
-	// }
+	if (firstNum > secondNum) {
+		fmt.Println("First Number is greater than the Second Number")
+	} else {
+		fmt.Println("Mehhh")
+	}
 
-	// fmt.Println(ipagDikit(firstName, lastName))
+	fmt.Println(ipagDikit(firstName, lastName))
 
 	type ninja struct {
 
@@ -74,6 +85,75 @@ func main(){
 
 	//PRACTICE
 
+	//for-each loop
+
+	s := "Hello World"
+
+	// for i, c := range s {
+	// 	fmt.Printf("%d %c", i, c)
+	// 	fmt.Println()
+	// }
+
+	for _, c:= range s {
+		fmt.Printf("%c", c)
+		fmt.Println()
+	}
+
+	listOfNames := []string{"kekw", "meow", "lmao"}
+
+	for i, names := range listOfNames {
+		fmt.Printf("%d) %v", i, names)
+		fmt.Println()
+	}
+
+	listOfSchool := []string{"DLSAU", "PUP", "UST", "UCC"}
+
+	for i, school := range listOfSchool {
+		fmt.Printf("%d) %v", i, school)
+		fmt.Println()
+	}
+	 
+	listOfCats := []string{"Alyanna", "Strok", "Bunso", "Tonky", "Tjoeki", "Ponkan", "Oreo", "Duryan", "Pabo"}
+
+	for _, cats := range listOfCats {
+		fmt.Printf(cats)
+		fmt.Println()
+	}
+
+	alyanna := catType {
+		name: "Alyanna",
+		color: "Black and White",
+		traits: []string{"Loves aircon", "Fierce", "Loyal"},
+	}
+
+	bunso := catType {
+		name: "Bunso",
+		color: "Orange",
+		traits: []string{"Noisy af", "Dont know how to fight", "Weird Tail"},
+	}
+
+	strok := catType {
+		name: "Strok",
+		color: "Orange",
+		traits: []string{"Asshole", "Loyal", "Wobbly", "Ugly ass tail"},
+	}
+
+	fmt.Println(alyanna.name)
+	fmt.Println(bunso.name)
+	fmt.Println(strok.name)
+	alyanna.catScratch()
+
+
+	//Pointers
+
+	// a := 21
+	// b := &a
+	// c := a
+	// fmt.Println(*b, c)
+	// *b = 5
+	// fmt.Println(a)
+	// a = 23
+	// fmt.Println(b)
 
 	
 
